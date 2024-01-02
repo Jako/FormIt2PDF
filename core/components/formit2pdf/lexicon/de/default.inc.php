65a63bb92397094a00bb647a5696bb6b4156f35a
Thomas Jakobi <buero@treehillstudio.de>
2024-01-02 14:02:16 +0100

    Added translation using Weblate (German)

---
+++ core/components/formit2pdf/lexicon/de/default.inc.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Default Lexicon Entries for FormIt2PDF
+ *
+ * @package formit2pdf
+ * @subpackage lexicon
+ */
+$_lang['formit2pdf'] = '';
+$_lang['formit2pdf.error_create_pdf'] = '';
+$_lang['formit2pdf.heading'] = '';
+$_lang['formit2pdf.intro'] = '';
