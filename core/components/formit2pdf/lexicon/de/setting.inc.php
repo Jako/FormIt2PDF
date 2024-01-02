f571bc0f6ef8c01aed1e45b2b371223c886d2aa5
Thomas Jakobi <buero@treehillstudio.de>
2024-01-02 13:42:55 +0100

    Added translation using Weblate (German)

---
+++ core/components/formit2pdf/lexicon/de/setting.inc.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Setting Lexicon Entries for FormIt2PDF
+ *
+ * @package formit2pdf
+ * @subpackage lexicon
+ */
+$_lang['area_pdf'] = '';
+$_lang['area_template'] = '';
+$_lang['setting_formit2pdf.author'] = '';
+$_lang['setting_formit2pdf.author_desc'] = '';
+$_lang['setting_formit2pdf.creator'] = '';
+$_lang['setting_formit2pdf.creator_desc'] = '';
+$_lang['setting_formit2pdf.styleTpl'] = '';
+$_lang['setting_formit2pdf.styleTpl_desc'] = '';
+$_lang['setting_formit2pdf.customFonts'] = '';
+$_lang['setting_formit2pdf.customFonts_desc'] = '';
+$_lang['setting_formit2pdf.customFontsFolder'] = '';
+$_lang['setting_formit2pdf.customFontsFolder_desc'] = '';
+$_lang['setting_formit2pdf.debug'] = '';
+$_lang['setting_formit2pdf.debug_desc'] = '';
+$_lang['setting_formit2pdf.defaultFont'] = '';
+$_lang['setting_formit2pdf.defaultFont_desc'] = '';
+$_lang['setting_formit2pdf.defaultFontSize'] = '';
+$_lang['setting_formit2pdf.defaultFontSize_desc'] = '';
+$_lang['setting_formit2pdf.format'] = '';
+$_lang['setting_formit2pdf.format_desc'] = '';
+$_lang['setting_formit2pdf.mgb'] = '';
+$_lang['setting_formit2pdf.mgb_desc'] = '';
+$_lang['setting_formit2pdf.mgf'] = '';
+$_lang['setting_formit2pdf.mgf_desc'] = '';
+$_lang['setting_formit2pdf.mgh'] = '';
+$_lang['setting_formit2pdf.mgh_desc'] = '';
+$_lang['setting_formit2pdf.mgl'] = '';
+$_lang['setting_formit2pdf.mgl_desc'] = '';
+$_lang['setting_formit2pdf.mgr'] = '';
+$_lang['setting_formit2pdf.mgr_desc'] = '';
+$_lang['setting_formit2pdf.mgt'] = '';
+$_lang['setting_formit2pdf.mgt_desc'] = '';
+$_lang['setting_formit2pdf.mode'] = '';
+$_lang['setting_formit2pdf.mode_desc'] = '';
+$_lang['setting_formit2pdf.mPDFMethods'] = '';
+$_lang['setting_formit2pdf.mPDFMethods_desc'] = '';
+$_lang['setting_formit2pdf.orientation'] = '';
+$_lang['setting_formit2pdf.orientation_desc'] = '';
+$_lang['setting_formit2pdf.ownerPassword'] = '';
+$_lang['setting_formit2pdf.ownerPassword_desc'] = '';
+$_lang['setting_formit2pdf.textTpl'] = '';
+$_lang['setting_formit2pdf.textTpl_desc'] = '';
+$_lang['setting_formit2pdf.permissions'] = '';
+$_lang['setting_formit2pdf.permissions_desc'] = '';
+$_lang['setting_formit2pdf.userPassword'] = '';
+$_lang['setting_formit2pdf.userPassword_desc'] = '';
