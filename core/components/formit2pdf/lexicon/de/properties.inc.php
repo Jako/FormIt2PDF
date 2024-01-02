99caa03aec383fa345947744e10ce9c0f8474532
Thomas Jakobi <buero@treehillstudio.de>
2024-01-02 14:05:25 +0100

    Added translation using Weblate (German)

---
+++ core/components/formit2pdf/lexicon/de/properties.inc.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Properties Lexicon Entries for FormIt2PDF
+ *
+ * @package formit2pdf
+ * @subpackage lexicon
+ */
+$_lang['area_pdf'] = '';
+$_lang['formit2pdf.formit2pdf.pdfAuthor'] = '';
+$_lang['formit2pdf.formit2pdf.pdfCreator'] = '';
+$_lang['formit2pdf.formit2pdf.pdfCustomFonts'] = '';
+$_lang['formit2pdf.formit2pdf.pdfCustomFontsFolder'] = '';
+$_lang['formit2pdf.formit2pdf.pdfDefaultFont'] = '';
+$_lang['formit2pdf.formit2pdf.pdfDefaultFontSize'] = '';
+$_lang['formit2pdf.formit2pdf.pdfFilename'] = '';
+$_lang['formit2pdf.formit2pdf.pdfFormat'] = '';
+$_lang['formit2pdf.formit2pdf.pdfMgb'] = '';
+$_lang['formit2pdf.formit2pdf.pdfMgf'] = '';
+$_lang['formit2pdf.formit2pdf.pdfMgh'] = '';
+$_lang['formit2pdf.formit2pdf.pdfMgl'] = '';
+$_lang['formit2pdf.formit2pdf.pdfMgr'] = '';
+$_lang['formit2pdf.formit2pdf.pdfMgt'] = '';
+$_lang['formit2pdf.formit2pdf.pdfMode'] = '';
+$_lang['formit2pdf.formit2pdf.pdfMPDFMethods'] = '';
+$_lang['formit2pdf.formit2pdf.pdfMultiSeparator'] = '';
+$_lang['formit2pdf.formit2pdf.pdfOrientation'] = '';
+$_lang['formit2pdf.formit2pdf.pdfOwnerPassword'] = '';
+$_lang['formit2pdf.formit2pdf.pdfPermissions'] = '';
+$_lang['formit2pdf.formit2pdf.pdfStyleTpl'] = '';
+$_lang['formit2pdf.formit2pdf.pdfTextTpl'] = '';
+$_lang['formit2pdf.formit2pdf.pdfTitle'] = '';
+$_lang['formit2pdf.formit2pdf.pdfUserPassword'] = '';
