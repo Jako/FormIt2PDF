46c5f40f43a11056374dac852195d4d1019e8ce3
Thomas Jakobi <buero@treehillstudio.de>
2024-01-02 13:05:15 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: MODX Extras/Standard
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/formit2pdf/standard/de/

--- core/components/formit2pdf/lexicon/de/default.inc.php
+++ core/components/formit2pdf/lexicon/de/default.inc.php
@@ -5,7 +5,7 @@
  * @package formit2pdf
  * @subpackage lexicon
  */
-$_lang['formit2pdf'] = '';
-$_lang['formit2pdf.error_create_pdf'] = '';
-$_lang['formit2pdf.heading'] = '';
-$_lang['formit2pdf.intro'] = '';
+$_lang['formit2pdf'] = 'FormIt2PDF';
+$_lang['formit2pdf.error_create_pdf'] = 'Der PDF-Anhang konnte nicht erstellt werden!';
+$_lang['formit2pdf.heading'] = 'Formular-Übermittlung';
+$_lang['formit2pdf.intro'] = 'Sie haben das Formular mit den folgenden Daten ausgefüllt:';
